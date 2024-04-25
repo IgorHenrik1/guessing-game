d1006ce9f437a53a9fa105c9bb6f2b0a513ff242
Igor <igorhenrikg1@gmail.com>
2024-04-25 11:45:43 -0300

    refactoring the code

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-const numeroSorteado = Math.round(Math.random() * 10);
-console.log(numeroSorteado);
-let tentativas = 1;
-
-function handleTryClick(e) {
-    e.preventDefault();
-    const inputNumber = document.querySelector('#inputNumber');
-
-    if (Number(inputNumber.value) === numeroSorteado) {
-        document.querySelector('.screen1').classList.add('hide');
-        document.querySelector('.screen2').classList.remove('hide');
-
-        tentativas > 1
-            ? (document.querySelector(
-                  '.screen2 h2',
-              ).innerText = `Acertou em ${tentativas} tentativas`)
-            : (document.querySelector(
-                  '.screen2 h2',
-              ).innerText = `Acertou em ${tentativas} tentativa`);
-    }
-
-    console.log(tentativas);
-
-    tentativas++;
-}
-
 const btnTry = document.querySelector('#btnTry');
 const btnReset = document.querySelector('#btnReset');
+const screen1 = document.querySelector('.screen1');
+const screen2 = document.querySelector('.screen2');
+const randomNumber = Math.round(Math.random() * 10);
+let attempts = 1;
+
+console.log(randomNumber);
 
 btnTry.addEventListener('click', handleTryClick);
 btnReset.addEventListener('click', function () {
     location.reload();
 });
+
+function handleTryClick(e) {
+    e.preventDefault();
+    const inputNumber = document.querySelector('#inputNumber');
+
+    if (Number(inputNumber.value) === randomNumber) {
+        screen1.classList.add('hide');
+        screen2.classList.remove('hide');
+
+        attempts > 1
+            ? (screen2.querySelector(
+                  'h2',
+              ).innerText = `Acertou em ${attempts} tentativas`)
+            : (screen2.querySelector(
+                  'h2',
+              ).innerText = `Acertou em ${attempts} tentativa`);
+    }
+
+    attempts++;
+}
